0dba67077cee3d07183b5fd3a885e0f45b47b590
agustin diambra <agustindiambra@gmail.com>
2022-08-24 21:13:20 -0300

    JS

---
+++ index.js
@@ -0,0 +1,12 @@
+
+for(let i = 0; i < 1; i++ ){
+    alert("hola", i);
+}
+
+
+
+for(let i = 0; i <=10; i++){
+    let nombre = prompt("ingrese su nombre");
+    let mensaje = `turno Nro ${i} nombre: ${nombre}`;
+    alert(mensaje);
+}
