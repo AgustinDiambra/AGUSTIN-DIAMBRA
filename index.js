67058d6f4e07a0c4a34646f26a38facce1669f7a
agustin diambra <agustindiambra@gmail.com>
2022-09-05 22:00:15 -0300

    js

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
+const cantidadProductos = () => {
+	let cantidad = Number(prompt('Ingrese cantidad de productos hasta 10'));
 
-for(let i = 0; i < 1; i++ ){
-    alert("hola", i);
-}
+	while (cantidad <= 0 || cantidad > 10) {
+		alert(
+			':( Ha introducido un valor para cantidad incorrecto, por favor introduzca un valor válido entre 1 y 100.'
+		);
+		cantidad = Number(prompt('Ingrese la cantidad de productos hasta 100'));
+	}
 
+	ingresarPrecio(cantidad);
+};
 
+const ingresarPrecio = (cantidad) => {
+	let gastoTotal = 0;
+	let precioProducto = 0;
+	// let contador = 0;
 
-for(let i = 0; i <=10; i++){
-    let nombre = prompt("ingrese su nombre");
-    let mensaje = `turno Nro ${i} nombre: ${nombre}`;
-    alert(mensaje);
-}
+	for (let i = 1; i <= cantidad; i++) {
+		precioProducto = Number(prompt('Ingrese el precio del producto' + i + ':'));
+
+		if (precioProducto > 0) {
+			gastoTotal = calcularTotal(gastoTotal, precioProducto);
+		} else {
+			while (precioProducto <= 0 || isNaN(precioProducto)) {
+				alert(
+					'Ha introducido un valor erroneo para el precio del producto. Por favor ingrese un valor superior a cero (0)'
+				);
+				precioProducto = Number(
+					prompt('Ingrese el precio del producto' + i + ':')
+				);
+			}
+			gastoTotal = calcularTotal(gastoTotal, precioProducto);
+		}
+	}
+
+	mostrarTotal(gastoTotal, cantidad);
+};
+
+const calcularTotal = (gastoTotal, precioProducto) =>
+	gastoTotal + precioProducto;
+
+const mostrarTotal = (gastoTotal, cantidad) => {
+	console.log('Cantidad de productos: ' + cantidad);
+	console.log('El precio total es $' + gastoTotal);
+};
+
+cantidadProductos();
